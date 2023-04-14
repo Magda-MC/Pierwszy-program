314b26eff62b517fe9688dc75957c05b7ad1426a
Magda - CM <magdalena.chrapowicz@gmail.com>
2023-04-14 03:53:21 +0200

    Pierwsza wersja programu.

---
+++ Pierwszy-program/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Kocham Muszelkę");
+Console.WriteLine("Brutal jest super kotem");
+Console.WriteLine("Kocham Misia marude");
+Console.WriteLine("Chcę się czegoś nauczyć");
+Console.WriteLine("Nie będę oszukiwać");
