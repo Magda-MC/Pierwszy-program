915d100082825583f909b7702534c77a3c2cc7fa
Magda - CM <magdalena.chrapowicz@gmail.com>
2023-04-16 17:21:05 +0200

    Pierwsza wesja programu.

--- Pierwszy-program/Program.cs
+++ Pierwszy-program/Program.cs
@@ -1,5 +1,25 @@
-﻿Console.WriteLine("Kocham Muszelkę");
-Console.WriteLine("Brutal jest super kotem");
-Console.WriteLine("Kocham Misia marude");
-Console.WriteLine("Chcę się czegoś nauczyć");
-Console.WriteLine("Nie będę oszukiwać");
+﻿//Tworzenie zmiennych bez deklaracji wartości
+using System.ComponentModel.Design;
+
+string imie;
+int wiek;
+bool plec;
+
+//Deklaracja wartości dla zmiennych
+wiek = 33;
+imie = "Ewa";
+plec = false;
+
+
+//Warunkowe wyświetlenie komunikatów
+
+if (imie == "Ewa" && wiek == 33 && plec == false)
+{ Console.WriteLine(imie + ", lat " + wiek); }
+else
+if (wiek <= 30 && plec == false)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else
+{ Console.WriteLine("Niepełnoletni mężczyzna"); }
+
